6330587edc6d06935e519b5bbfe01ba486fac424
kml <kamielklumpers@gmail.com>
2023-01-04 20:19:11 +0100

    Remove authentication (for now) and authorization

--- DominionScoreboard/Program.cs
+++ DominionScoreboard/Program.cs
@@ -2,9 +2,7 @@ using System.Text.Json;
 using Dominion.Services;
 using DominionScoreboard.Data;
 using DominionScoreboard.Games;
-using DominionScoreboard.Models;
 using FluentValidation;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,15 +12,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
 
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(opt =>
@@ -55,10 +44,6 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthentication();
-app.UseIdentityServer();
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
